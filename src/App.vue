067f3d9497c8405c5146076cac7c644f87ed5db6
Conor Holgate <conor.holgate@lesar.co.uk>
2022-05-23 07:53:41 +0100

    copyright message added on footer

--- src/App.vue
+++ src/App.vue
@@ -174,6 +174,13 @@
             </div>
         </div>
     </main>
+    <footer class="absolute bottom-0 px-4 py-2">
+        <div class="flex justify-center text-gray-400">
+            <div>
+                <span>© 2022 Copyright All Rights Reserved</span>
+            </div>
+        </div>
+    </footer>
   </div>
 </template>
 
